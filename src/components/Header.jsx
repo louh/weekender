b5d0911a96de06c9a5bb529021373ddc245c0e21
Lou Huang <lou@louhuang.com>
2018-11-08 15:04:17 -0500

    Separate <Nav /> component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Nav from './Nav'
 
 import IMG_WEEKENDER_TITLE from '../../images/weekender.svg'
 import './Header.css'
@@ -12,28 +13,7 @@ export default class Header extends Component {
           <h1>The Weekender</h1>
           <img className="header-image" src={IMG_WEEKENDER_TITLE} alt="The Weekender" />
         </div>
-        <div className="header-container">
-          <div className="header-left">
-            {/* Do not use images here like in the original, it facilitates localization */}
-            <ul>
-              <li><a href="">Service Status</a></li>
-              <li><a href="">Service by Station</a></li>
-              <li><a href="">Service by Line</a></li>
-              <li><a href="">Service by Borough</a></li>
-              <li><a href="http://tripplanner.mta.info/" target="_blank" rel="noopener noreferrer">Trip Planner+</a></li>
-              <li><a href="http://mta.info/lirr" target="_blank" rel="noopener noreferrer">LIRR</a> / <a href="http://www.mta.info/mnr" target="_blank" rel="noopener noreferrer">Metro-North</a></li>
-              <li><a href="http://mta.info/nyct/" target="_blank" rel="noopener noreferrer">Buses</a></li>
-              <li><a href="http://www.mta.info/bandt" target="_blank" rel="noopener noreferrer">Bridges & Tunnels</a></li>
-            </ul>
-          </div>
-          <div className="header-right">
-            <br /><br /><br />
-            <ul>
-              <li><a href="">Subway Diagram</a></li>
-              <li className="float-right"><a href="http://www.mta.info/" target="_blank" rel="noopener noreferrer">MTA Home</a></li>
-            </ul>
-          </div>
-        </div>
+        <Nav />
       </header>
     )
   }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Nav.css'
+
+export default class Nav extends Component {
+  render () {
+    return (
+      <nav className="nav-container">
+        <div className="nav-left">
+          {/* Do not use images here like in the original, it facilitates localization */}
+          <ul>
+            <li><Link to="/">Service Status</Link></li>
+            <li><Link to="/station">Service by Station</Link></li>
+            <li><Link to="/line">Service by Line</Link></li>
+            <li><Link to="/borough">Service by Borough</Link></li>
+            <li><a href="http://tripplanner.mta.info/" target="_blank" rel="noopener noreferrer">Trip Planner+</a></li>
+            <li><a href="http://mta.info/lirr" target="_blank" rel="noopener noreferrer">LIRR</a> / <a href="http://www.mta.info/mnr" target="_blank" rel="noopener noreferrer">Metro-North</a></li>
+            <li><a href="http://mta.info/nyct/" target="_blank" rel="noopener noreferrer">Buses</a></li>
+            <li><a href="http://www.mta.info/bandt" target="_blank" rel="noopener noreferrer">Bridges & Tunnels</a></li>
+          </ul>
+        </div>
+        <div className="nav-right">
+          <br /><br /><br />
+          <ul>
+            <li><a href="">Subway Diagram</a></li>
+            <li className="float-right"><a href="http://www.mta.info/" target="_blank" rel="noopener noreferrer">MTA Home</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
