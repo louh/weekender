8759928c166e19676f113e0bfc026ae6a1bd2a55
Lou Huang <lou@louhuang.com>
2018-11-09 21:18:42 -0500

    Read all data files from S3

--- src/index.js
+++ src/index.js
@@ -6,26 +6,30 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 
-// Test proxy
-// window.fetch('http://35.196.121.180/mta/status/weekendboroughstatus.js')
-const scriptPromise = new Promise((resolve, reject) => {
-  const script = document.createElement('script')
-  document.body.appendChild(script)
-  script.onload = resolve
-  script.onerror = reject
-  script.async = true
-  script.src = '/fixtures/weekendstatus.js'
-})
+const FILES = [
+  'https://s3.amazonaws.com/weekender-data/weekendstatus.js',
+  'https://s3.amazonaws.com/weekender-data/weekendroutestatus.js',
+  'https://s3.amazonaws.com/weekender-data/weekendboroughstatus.js'
+]
+
+function embedScript (url) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script')
+    document.body.appendChild(script)
+    script.onload = resolve
+    script.onerror = reject
+    script.async = true
+    script.src = url
+  })
+}
 
 const mountNode = document.getElementById('app')
 
-scriptPromise.then(() => {
-  console.log('test data loaded')
-
+Promise.all(FILES.map(embedScript)).then(() => {
   // Render only after data loads, for now.
   ReactDOM.render(
     <Router>
       <App />
     </Router>
   , mountNode)
-  })
+})
